25b1bd65dc8e03dbb070a927e7302d4bf191b1aa
Von-Feonix <527171978@qq.com>
2022-01-29 15:07:41 +1100

    login
    login

---
+++ src/App.tsx
@@ -0,0 +1,73 @@
+import { Form, Input, Button, Checkbox, Radio, RadioChangeEvent } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import "./App.css";
+import { Header } from "antd/lib/layout/layout";
+
+
+
+function App() {
+  const onFinish = (values: any) => {
+    console.log("Received values of form: ", values);
+  };
+  return (
+    
+    <>
+    <Header/>
+      <h1 className="login-title">Course Management Assistant</h1>
+      <div className="login-container">
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+        >
+          <Form.Item name="role">
+            <Radio.Group defaultValue={'student'}>
+              <Radio.Button value="student">Student</Radio.Button>
+              <Radio.Button value="teacher">Teacher</Radio.Button>
+              <Radio.Button value="manager">Manager</Radio.Button>
+            </Radio.Group>
+          </Form.Item>
+
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: "Please input your Username!" }]}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Username"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Please input your Password!" }]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              Log in
+            </Button>
+            No account? &nbsp;&nbsp; <a href="">Sign up</a>
+          </Form.Item>
+        </Form>
+      </div>
+    </>
+  );
+}
+
+export default App;
